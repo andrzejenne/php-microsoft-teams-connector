af64a093d234ec99fc5b7c35e61836991cb03552
Andrzej Heczko <andrzej.heczko@quanti.cz>
2022-01-04 11:43:58 +0100

    wip: refactor

--- src/Cards/Adaptive/Containers/Container.php
+++ src/Cards/Adaptive/Containers/Container.php
@@ -3,6 +3,11 @@
 namespace Sebbmyr\Teams\Cards\Adaptive\Containers;
 
 use Sebbmyr\Teams\Cards\Adaptive\AbstractElement;
+use Sebbmyr\Teams\Cards\Adaptive\Elements\AdaptiveCardElement;
+use Sebbmyr\Teams\Cards\Adaptive\Traits\HasBackgroundImage;
+use Sebbmyr\Teams\Cards\Adaptive\Traits\HasBleed;
+use Sebbmyr\Teams\Cards\Adaptive\Traits\HasMinHeight;
+use Sebbmyr\Teams\Cards\Adaptive\Traits\HasStyle;
 
 /**
  * Container
@@ -12,6 +17,8 @@ use Sebbmyr\Teams\Cards\Adaptive\AbstractElement;
  */
 class Container extends AbstractElement
 {
+    use HasBackgroundImage, HasBleed, HasMinHeight, HasStyle;
+
     /**
      * The value of the fact.
      * Required: yes
@@ -19,12 +26,32 @@ class Container extends AbstractElement
      * @var string
      */
     private $items;
-    
+
+    /**
+     * @param array $items
+     * @return static
+     */
+    public static function create(array $items = []): self
+    {
+        return new static($items);
+    }
+
+    /**
+     * @param array $items
+     */
     public function __construct(array $items)
     {
         parent::__construct('Container');
 
         $this->items = $items;
+    }
+
+    /**
+     * @param AdaptiveCardElement $element
+     */
+    public function addItem(AdaptiveCardElement $element)
+    {
+        $this->items[] = $element;
     }
 
     /**
@@ -35,8 +62,25 @@ class Container extends AbstractElement
      */
     public function jsonSerialize()
     {
-        return parent::jsonSerialize() + [
+        $data = parent::jsonSerialize() + [
             'items' => $this->items,
         ];
+
+        if ($this->version >= 1.2) {
+            if (is_bool($this->bleed)) {
+                $data['bleed'] = $this->bleed;
+            }
+            if (!empty($this->backgroundImage)) {
+                $data['backgroundImage'] = $this->backgroundImage;
+            }
+            if (!empty($this->minHeight)) {
+                $data['minHeight'] = $this->minHeight;
+            }
+            if (!empty($this->style)) {
+                $data['style'] = $this->style;
+            }
+        }
+
+        return $data;
     }
 }
